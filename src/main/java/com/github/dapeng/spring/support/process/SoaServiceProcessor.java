80e5f0b02b3d62b54b7b7623f536159c48234c11
wxwang <wxwang@today36524.com.cn>
2019-12-03 15:11:36 +0800

    修改

--- src/main/java/com/github/dapeng/spring/support/process/SoaServiceProcessor.java
+++ src/main/java/com/github/dapeng/spring/support/process/SoaServiceProcessor.java
@@ -4,8 +4,6 @@ import com.github.dapeng.spring.SoaProcessorFactory;
 import com.github.dapeng.spring.support.annotation.SoaService;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -26,11 +24,8 @@ public class SoaServiceProcessor implements InitializingBean, ApplicationContext
         for (String beanName : beanNamesForAnnotation) {
             String id = beanName + "_SoaProcessor";
             DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) configurableApplicationContext.getBeanFactory();
-            BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(SoaProcessorFactory.class);
-            beanDefinitionBuilder.addConstructorArgValue(configurableApplicationContext.getBean(beanName));
-            beanDefinitionBuilder.addConstructorArgValue(beanName);
-            AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
-            defaultListableBeanFactory.registerBeanDefinition(id, beanDefinition);
+            SoaProcessorFactory soaProcessorFactory = new SoaProcessorFactory(defaultListableBeanFactory.getBean(beanName), beanName);
+            defaultListableBeanFactory.registerSingleton(id, soaProcessorFactory);
         }
     }
 }
